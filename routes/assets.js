a4e52670f3a3601fb9dc7cecefd6d097605621a5
Carlos Chacon <cchacon266@gmail.com>
2024-05-08 01:43:57 -0600

    agregando status de sesión de inventario

--- routes/assets.js
+++ routes/assets.js
@@ -8,6 +8,27 @@ router.get('/', async (req, res) => {
         const assets = await mongoose.connection.db.collection('assets').find({}).toArray();
         const total = assets.length; // Calcula el total de documentos
 
+        // Obtener información del empleado asignado para cada activo
+        for (const asset of assets) {
+            if (asset.assigned) {
+                const employee = await mongoose.connection.db.collection('employees').findOne({ _id: mongoose.Types.ObjectId(asset.assigned) });
+                if (employee) {
+                    // Agregar campos employee_id y employee_name al objeto de activo
+                    asset.employee_id = employee.employee_id;
+                    asset.employee_name = `${employee.name} ${employee.lastName}`;
+                }
+            }
+            // Obtener información de la ubicación para cada activo
+            if (asset.location) {
+                const location = await mongoose.connection.db.collection('locationsReal').findOne({ _id: mongoose.Types.ObjectId(asset.location) });
+                if (location) {
+                    // Agregar campos location_Name y location_Level al objeto de activo
+                    asset.location_Name = location.name;
+                    asset.location_Level = location.profileLevel;
+                }
+            }
+        }
+
         // Estructura la respuesta como se describe
         const response = {
             platform: {
@@ -21,7 +42,7 @@ router.get('/', async (req, res) => {
                 method: 'GET',
                 total: total
             },
-            response: assets // Los documentos de la colección
+            response: assets // Los documentos de la colección, ahora con los campos employee_id, employee_name, location_Name y location_Level agregados
         };
 
         // Convierte la respuesta a JSON con formato legible
@@ -34,6 +55,8 @@ router.get('/', async (req, res) => {
         res.status(500).json({ error: 'Ocurrió un error al obtener los documentos de la colección.' });
     }
 });
+
+
 
 module.exports = router;
 
